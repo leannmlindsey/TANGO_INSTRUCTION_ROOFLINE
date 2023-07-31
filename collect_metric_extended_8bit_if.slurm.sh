94fca26656f87d6f23438490092eb7f6bac6cc7d
leannmlindsey <leannmlindsey@gmail.com>
2023-07-31 03:35:02 -0600

    added all files from Instruction Rooflines

---
+++ collect_metric_extended_8bit_if.slurm.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --mem=0
+#SBATCH --account=soc-gpu-np
+#SBATCH --partition=soc-gpu-np
+#SBATCH --job-name=roof_8bit_if
+#SBATCH --time=4:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --ntasks=1
+#SBATCH --output=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/FINAL_BENCHMARKING_SCRIPTS/roof_8bit_if%j.out
+
+echo "CPU Node Information"
+uname -a
+echo "GPU Node Information"
+
+nvidia-smi
+module load cuda
+
+set -x
+
+# print usage
+#function usage() {
+#    echo "USAGE: sudo collect_metrics_extended.sh"
+#    echo "DEPENDENCY: GiCOPS setup and ready to run"
+#    echo "BEFORE RUNNING: Set GiCOPS_HOME path on line# 22, and params (except dM) on line# 28"
+#}
+
+# make sure of sudo acess
+#if [ "$(id -u)" != "0" ] ; then
+#   echo "ERROR: Run this script as sudo"
+#   usage
+#   exit 0
+#fi
+
+# list of kernels to profile
+kernel=(sequence_dna_kernel_traceback)
+
+# path to app's home
+#apphome=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/LEANN_TEST/TESTING_ADEPT_8BIT/ADEPT-T
+
+# ADEPT GPU-BSW
+#apphome=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/LEANN_TEST/ADEPT_DEVELOPMENT/DEVELOPMENT/GPU-BSW
+
+# ADEPT 4 bit
+#apphome=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/LEANN_TEST/ADEPT_CURRENT/CLEAN/ADEPT-T
+
+# ADEPT 8 bit vertical 
+#apphome=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/LEANN_TEST/ADEPT_CURRENT/8BIT_VERTICAL/ADEPT-T
+
+# ADEPT 8 bit vertical if 
+apphome=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/LEANN_TEST/ADEPT_CURRENT/8BIT_VERTICAL_IF/ADEPT-T
+
+# path to app and arguments
+app=${apphome}/build/program_gpu
+
+# arguments
+arg1=dna
+arg2=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/DATA/large_datasets/DNA/MILLION/dna_ref_1M.fasta
+arg3=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/DATA/large_datasets/DNA/MILLION/dna_read_1M.fasta
+arg4=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/BENCHMARKING_RESULTS/ROOFLINE/out
+
+#outputfolder=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/BENCHMARKING_RESULTS/ROOFLINE/ADEPT
+#outputfolder=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/BENCHMARKING_RESULTS/ROOFLINE/ADEPT_4bit
+#outputfolder=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/BENCHMARKING_RESULTS/ROOFLINE/ADEPT_8bit
+outputfolder=/uufs/chpc.utah.edu/common/home/u1323098/sundar-group-space2/ADEPT/BENCHMARKING_RESULTS/ROOFLINE/ADEPT_8bit_if_2
+
+mkdir $outputfolder
+
+# prepare the app
+#pushd ${apphome}/build
+#cmake .. -DUSE_GPU=ON -DUSE_MPI=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${apphome}/install-ncu
+#make clean
+#make install -j 12
+#popd
+
+# remove previous output directory if exists
+rm -rf ${outputfolder}
+
+# make a new output directory
+mkdir -p ${outputfolder}
+
+
+# timing for kernels
+ncu --print-summary per-gpu ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/clean.log
+
+# collect all metrics for all kernels individually
+for k in ${kernel[@]}
+do
+    echo "Profiling kernel: ${k}"
+
+    # events not available
+    ncu -k "${k}" --csv --metrics smsp__thread_inst_executed.sum,smsp__inst_executed.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_event.log
+
+    ncu -k "${k}" --csv --metrics smsp__inst_executed.avg.per_cycle_active,smsp__sass_thread_inst_executed_op_integer_pred_on.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_set1.log
+    ncu -k "${k}" --csv --metrics smsp__inst_executed_op_global_ld.sum,smsp__inst_executed_op_global_st.sum,smsp__inst_executed_op_local_ld.sum,smsp__inst_executed_op_local_st.sum,dram__sectors_read.sum,dram__sectors_write.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_inst.log
+    ncu -k "${k}" --csv --metrics l1tex__data_pipe_lsu_wavefronts_mem_shared_op_ld.sum,l1tex__data_pipe_lsu_wavefronts_mem_shared_op_st.sum,smsp__inst_executed_op_shared_ld_pred_on_any.sum,smsp__inst_executed_op_shared_ld_pred_off_all.sum,smsp__inst_executed_op_shared_st_pred_on_any.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_inst_shared.log
+    ncu -k "${k}" --csv --metrics smsp__sass_thread_inst_executed_op_memory_pred_on.sum,smsp__inst_executed_pipe_lsu.avg.pct_of_peak_sustained_active ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_ld.log
+    # ncu -k "${k}" --csv --metrics smsp__sass_thread_inst_executed_op_conversion_pred_on.sum,smsp__sass_thread_inst_executed_op_control_pred_on.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_set2.log
+    # ncu -k "${k}" --csv --metrics smsp__sass_thread_inst_executed_op_fp64_pred_on.sum,smsp__sass_thread_inst_executed_op_fp32_pred_on.sum,smsp__sass_thread_inst_executed_op_fp16_pred_on.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_set3.log
+    # ncu -k "${k}" --csv --metrics smsp__sass_thread_inst_executed_op_dadd_pred_on.sum,smsp__sass_thread_inst_executed_op_dmul_pred_on.sum,smsp__sass_thread_inst_executed_op_fadd_pred_on.sum,smsp__sass_thread_inst_executed_op_fmul_pred_on.sum,smsp__sass_thread_inst_executed_op_ffma_pred_on.sum,smsp__sass_thread_inst_executed_op_hadd_pred_on.sum,smsp__sass_thread_inst_executed_op_hmul_pred_on.sum,smsp__sass_thread_inst_executed_op_hfma_pred_on.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_set4.log
+    # ncu -k "${k}" --csv --metrics smsp__sass_thread_inst_executed_op_dfma_pred_on.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_flop_count_dp_fma.log
+    ncu -k "${k}" --csv --metrics l1tex__t_sectors_pipe_lsu_mem_local_op_ld.sum,l1tex__t_sectors_pipe_lsu_mem_local_op_st.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_local.log
+    ncu -k "${k}" --csv --metrics l1tex__t_sectors_pipe_lsu_mem_global_op_st.sum,l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_global.log
+    # ncu -k "${k}" --csv --metrics l1tex__average_t_sectors_per_request_pipe_lsu_mem_local_op_ld.ratio,l1tex__average_t_sectors_per_request_pipe_lsu_mem_local_op_st.ratio ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_local_req.log
+    # ncu -k "${k}" --csv --metrics l1tex__average_t_sectors_per_request_pipe_lsu_mem_global_op_ld.ratio,l1tex__average_t_sectors_per_request_pipe_lsu_mem_global_op_st.ratio ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_gld_req.log
+    ncu -k "${k}" --csv --metrics lts__t_sectors_op_write.sum,lts__t_sectors_op_atom.sum,lts__t_sectors_op_red.sum,lts__t_sectors_op_read.sum,lts__t_sectors_op_atom.sum,lts__t_sectors_op_red.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_l2.log
+    # ncu -k "${k}" --csv --metrics lts__t_sectors_aperture_sysmem_op_read.sum,lts__t_sectors_aperture_sysmem_op_write.sum ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_sysmem.log
+    # ncu -k "${k}" --csv --metrics smsp__sass_average_branch_targets_threads_uniform.pct ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_branch_efficiency.log
+    # ncu -k "${k}" --csv --metrics smsp__thread_inst_executed_per_inst_executed.pct,smsp__thread_inst_executed_per_inst_executed.ratio ${app} ${arg1} ${arg2} ${arg3} ${arg4} |&  tee ${outputfolder}/${k}_warp_execu_eff.log
+
+done
+
---
+++ merge.sh
@@ -0,0 +1,58 @@
+#!/bin/bash 
+echo "Started running the merge"
+
+# print usage
+function usage() {
+    echo "USAGE: merge.sh </path/to/extended_output>"
+}
+
+# if no input params
+if [ -z "$1" ]; then 
+    usage
+    exit -1
+fi
+
+# extended_output path
+OPATH=${1}
+
+# list of kernels
+kernels=(sequence_dna_kernel_traceback)
+
+# list of metrics
+metric=(smsp__inst_executed.avg.per_cycle_active smsp__inst_executed.sum smsp__sass_thread_inst_executed_op_integer_pred_on.sum smsp__sass_thread_inst_executed_op_memory_pred_on.sum smsp__inst_executed_op_global_st.sum smsp__inst_executed_op_local_ld.sum smsp__inst_executed_op_local_st.sum smsp__inst_executed_op_shared_ld_pred_on_any.sum smsp__inst_executed_op_shared_st_pred_on_any.sum smsp__inst_executed_pipe_lsu.avg.pct_of_peak_sustained_active smsp__sass_thread_inst_executed_op_conversion_pred_on.sum smsp__sass_thread_inst_executed_op_control_pred_on.sum smsp__sass_thread_inst_executed_op_fp64_pred_on.sum smsp__sass_thread_inst_executed_op_fp32_pred_on.sum smsp__sass_thread_inst_executed_op_fp16_pred_on.sum smsp__sass_thread_inst_executed_op_dadd_pred_on.sum smsp__sass_thread_inst_executed_op_dmul_pred_on.sum smsp__sass_thread_inst_executed_op_dfma_pred_on.sum smsp__sass_thread_inst_executed_op_fadd_pred_on.sum smsp__sass_thread_inst_executed_op_fmul_pred_on.sum smsp__sass_thread_inst_executed_op_ffma_pred_on.sum  smsp__sass_thread_inst_executed_op_hadd_pred_on.sum smsp__sass_thread_inst_executed_op_hmul_pred_on.sum smsp__sass_thread_inst_executed_op_hfma_pred_on.sum l1tex__t_sectors_pipe_lsu_mem_local_op_ld.sum l1tex__t_sectors_pipe_lsu_mem_local_op_st.sum l1tex__data_pipe_lsu_wavefronts_mem_shared_op_ld.sum l1tex__data_pipe_lsu_wavefronts_mem_shared_op_st.sum l1tex__t_sectors_pipe_lsu_mem_global_op_st.sum l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum l1tex__average_t_sectors_per_request_pipe_lsu_mem_local_op_ld.ratio l1tex__average_t_sectors_per_request_pipe_lsu_mem_local_op_st.ratio l1tex__average_t_sectors_per_request_pipe_lsu_mem_global_op_ld.ratio l1tex__average_t_sectors_per_request_pipe_lsu_mem_global_op_st.ratio smsp__inst_executed_op_global_red.sum smsp__inst_executed_op_global_ld.sum  lts__t_sectors_op_write.sum lts__t_sectors_op_read.sum lts__t_sectors_op_atom.sum lts__t_sectors_op_red.sum dram__sectors_read.sum dram__sectors_write.sum lts__t_sectors_aperture_sysmem_op_read.sum lts__t_sectors_aperture_sysmem_op_write.sum smsp__sass_average_branch_targets_threads_uniform.pct smsp__thread_inst_executed_per_inst_executed.pct smsp__thread_inst_executed_per_inst_executed.ratio) 
+
+# list of events
+event=(sm__inst_executed.sum smsp__thread_inst_executed.sum)
+
+# extract and append all of them in one file
+for kernel in ${kernels[@]}
+do
+	echo ${kernel}
+	filename="${OPATH}/${kernel}"
+	echo ${filename}
+
+	# remove previous file if exists
+	rm -rf ${filename}
+
+	# write all metrics
+	for m in ${metric[@]}
+	do
+		echo $m
+		data=`grep -rin -E "${kernel}.*\"${m}\"" ${OPATH}/${kernel}_*.log | awk -F'",' '{print $NF}' | sed 's/,//g' | sed 's/\"//g'`  
+		echo "${m},${data}" >> ${filename}.csv
+	done
+
+	# write events
+	for e in ${event[@]}
+	do
+		echo $e
+		data=`grep -rin -E "${kernel}.*\"${e}\"" ${OPATH}/${kernel}_event.log | awk -F'",' '{print $NF}' | sed 's/,//g' | sed 's/\"//g'`  
+		echo "${e},${data}" >> ${filename}.csv
+	done
+
+	# replace all spaces with comma to make a csv
+	sed -i "s/,[[:blank:]]*$//g" ${filename}.csv
+
+	# remove any trailing whitespaces
+	sed -i 's/[[:blank:]]*$//' ${filename}.csv
+done
